78141ce2fba954c5bdb886f78b261c68e349a371
Du Yizhuo <dyzdyz010@sina.com>
2016-04-27 16:35:17 +0800

    增加作品路由、页面，集成Disqus，样式修改

--- routes/handlers/pages.js
+++ routes/handlers/pages.js
@@ -5,3 +5,11 @@ exports.index = function (req, res) {
 exports.productions = function (req, res) {
     res.render('productions', {});
 };
+
+exports.productionDetail = function (req, res) {
+        res.render('production-detail', {pageUrl: req.originalUrl, pageIdentifier: req.params.production_id});
+};
+
+exports.resources = function (req, res) {
+        res.render('resources', {});
+};
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var pages = require('./handlers/pages');
 /* GET home page. */
 router.get('/', pages.index);
 router.get('/productions', pages.productions);
+router.get('/productions/:production_id', pages.productionDetail);
+router.get('/resources', pages.resources);
 
 
 module.exports = router;
