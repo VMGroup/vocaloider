8adca4e45e6e76bb5b70e872c40fde874fe40ce9
Du Yizhuo <dyzdyz010@sina.com>
2016-05-04 18:36:44 +0800

    Passport 登录认证

--- config/passport.js
+++ config/passport.js
@@ -4,10 +4,20 @@ var bcrypt = require('bcrypt');
 var User = require('../models/user');
 
 passport.use('local-login', new LoaclStrategy(
-    function (username, password, done) {
+    {
+        passReqToCallback: true
+    },
+    function (req, username, password, done) {
         User.findOne({username: username}, function (err, user) {
             if (err) return done(err);
-            if (!user) return done(null, false);
+            // Not found
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'No user with this username found.'));
+            }
+            // Wrong password
+            if (!bcrypt.compareSync(password, user.password)) {
+                return done(null, false, req.flash('loginMessage', 'Wrong username/password.'));
+            }
         });
     }
 ));
