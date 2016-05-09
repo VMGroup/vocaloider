1d6e5c241fdbcbb03b6185ea4073a5f4e7b3da01
Du Yizhuo <dyzdyz010@sina.com>
2016-05-09 17:39:29 +0800

    首页微调

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var pages = require('./handlers/pages');
 
+router.get('/', function (req, res) {
+	res.redirect('/admin/works');
+});
 router.get('/works', pages.admin_works);
 router.get('/resources', pages.admin_resources);
 
