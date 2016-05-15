1dc00bf9bc1919b6c03f14212687bfd770849bef
Du Yizhuo <dyzdyz010@sina.com>
2016-05-15 19:50:47 +0800

    添加Promise依赖

--- routes/handlers/admin.js
+++ routes/handlers/admin.js
@@ -1,10 +1,15 @@
 var Works = require('../../models/works');
 
 exports.AddWorks = function (req, res) {
+
     var works = new Works();
     works.title = req.body.title;
     works.description = req.body.description;
     works.date = new Date();
+    works.uploader = req.user._id;
+    console.log(works);
+
+    res.render('admin/works');
 };
 
 exports.DeleteWorks = function (req, res) {
