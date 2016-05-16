30de4810da067a9bb0a8949cc967edcef27682bf
Du Yizhuo <dyzdyz010@sina.com>
2016-05-16 22:46:44 +0800

    添加作品页面修改中

--- static/js/modal.js
+++ static/js/modal.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $(this).parents('.is-active').removeClass('is-active');
     });
 
-    $('form .modal-card-head .delete, .modal-card-foot .button').click(function() {
+    $('form .modal-card-head .delete, .modal-card-foot .button.cancel').click(function() {
         var modal = $(this).parents('.is-active');
         console.log(modal);
         $('html').removeClass('is-clipped');
